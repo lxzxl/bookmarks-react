804d7fe82063d7bc478c4a933bf0988ca6e4b1a0
lxzxl <lxz318@aliyun.com>
2017-11-23 10:48:22 +0800

    simplify action enum.

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import * as Icons from 'react-feather';
 
 export enum ActionType {
-    None = 0,
-    Save = 1,
-    Delete = -1,
+    None,
+    Save,
+    Delete,
 }
 
 interface Props {
@@ -16,18 +16,18 @@ export default function({isEditMode = false, handleAction}: Props) {
     return (
         <div className="Actions field is-grouped">
             {!isEditMode ?
-                <a className="button is-white" onClick={e => handleAction(0)}>
+                <a className="button is-white" onClick={e => handleAction(ActionType.None)}>
                     <span className="icon"><Icons.Settings size={16}/></span>
                 </a>
                 :
                 <div className="field is-grouped">
-                    <a className="button is-white" onClick={e => handleAction(1)}>
+                    <a className="button is-white" onClick={e => handleAction(ActionType.Save)}>
                         <span className="icon has-text-success"><Icons.Save size={16}/></span>
                     </a>
-                    <a className="button is-white" onClick={e => handleAction(-1)}>
+                    <a className="button is-white" onClick={e => handleAction(ActionType.Delete)}>
                         <span className="icon has-text-danger"><Icons.Trash2 size={16}/></span>
                     </a>
-                    <a className="button is-white" onClick={e => handleAction(0)}>
+                    <a className="button is-white" onClick={e => handleAction(ActionType.None)}>
                         <span className="icon"><Icons.X size={16}/></span>
                     </a>
                 </div>
