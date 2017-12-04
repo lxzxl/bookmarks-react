e624870d31b654bcc0dc26311528b5083c999888
lxzxl <lxz318@aliyun.com>
2017-12-04 14:22:01 +0800

    fix input check logic.

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -28,7 +28,7 @@ interface Props extends InputProps {
     handleChange: React.ChangeEventHandler<HTMLInputElement>;
     handleKeyUp?: React.KeyboardEventHandler<HTMLInputElement>;
 
-    checkValidateStatus?(isValid: boolean): void;
+    setValidateStatus?(hasError: boolean): void;
 }
 
 interface State {
@@ -40,50 +40,71 @@ export class InputField extends React.Component<Props, State> {
 
     constructor(props: Props) {
         super(props);
-        const {rules = []} = this.props;
+        const { rules = [] } = this.props;
         this.state = {
-            hasError: false,
+            hasError: false
         };
         this.isRequired = rules.indexOf(Rules.Required) > -1;
     }
 
+    componentDidMount() {
+        this.validate(this.props.value);
+    }
+
+    validate(value: string) {
+        if (this.isRequired) {
+            this.setState({ hasError: !value });
+        }
+        if (this.props.setValidateStatus) {
+            this.props.setValidateStatus(!value);
+        }
+    }
+
     render() {
-        const {fieldClass, controlClass, PreIcon, suffixIcon} = this.props;
-        const {type = 'text', ...inputProps} = pick(this.props, InputFields);
-        const {hasError} = this.state;
-        const errorCLass = hasError && 'is-danger' || '';
+        const { fieldClass, controlClass, PreIcon, suffixIcon } = this.props;
+        const { type = 'text', ...inputProps } = pick(this.props, InputFields);
+        const { hasError } = this.state;
+        const errorCLass = (hasError && 'is-danger') || '';
         return (
             <div className={`field ${fieldClass || ''}`}>
                 <div className={`control  ${controlClass || ''}`}>
-                    <input className={`input ${errorCLass}`} type={type}
-                           onChange={this.handleChange}
-                           onKeyUp={this.handleKeyUp}
-                           {...inputProps}/>
-                    {PreIcon && <span className="icon is-small is-left">{<PreIcon/>}</span>}
-                    {suffixIcon && <span className="icon is-small is-right">{suffixIcon}</span>}
+                    <input
+                        className={`input ${errorCLass}`}
+                        type={type}
+                        onChange={this.handleChange}
+                        onKeyUp={this.handleKeyUp}
+                        {...inputProps}
+                    />
+                    {PreIcon && (
+                        <span className="icon is-small is-left">
+                            {<PreIcon />}
+                        </span>
+                    )}
+                    {suffixIcon && (
+                        <span className="icon is-small is-right">
+                            {suffixIcon}
+                        </span>
+                    )}
                 </div>
-                {this.isRequired && <p className={`help ${errorCLass}`}>
-                    This field is required
-                </p>}
+                {this.isRequired && (
+                    <p className={`help ${errorCLass}`}>
+                        This field is required
+                    </p>
+                )}
             </div>
         );
     }
 
-    handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-        if (this.isRequired) {
-            this.setState({hasError: !event.target.value});
-        }
-        if (this.props.checkValidateStatus) {
-            this.props.checkValidateStatus(!!event.target.value);
-        }
+    handleChange: React.ChangeEventHandler<HTMLInputElement> = event => {
+        this.validate(event.target.value);
         if (this.props.handleChange) {
             this.props.handleChange(event);
         }
-    }
+    };
 
-    handleKeyUp: React.KeyboardEventHandler<HTMLInputElement> = (event) => {
+    handleKeyUp: React.KeyboardEventHandler<HTMLInputElement> = event => {
         if (this.props.handleKeyUp) {
             this.props.handleKeyUp(event);
         }
-    }
+    };
 }
--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import * as Icons from 'react-feather';
 import clone from 'lodash/clone';
-import pick from 'lodash/pick';
 import Notification from '../components/Notification';
-import {Rules, InputField} from '../components/InputField';
+import { Rules, InputField } from '../components/InputField';
 
 interface Account {
     email: string;
@@ -26,72 +25,80 @@ const demoAccount = {
 export default class Login extends React.Component<Props, State> {
     static getDefaultAccount(): Account {
         const lastEmail = localStorage.getItem('lastEmail');
-        if (lastEmail) {
-            return {
-                email: lastEmail,
-                password: ''
-            };
+        const account = clone(demoAccount);
+        if (lastEmail && lastEmail !== account.email) {
+            account.email = lastEmail;
+            account.password = '';
         }
-        return clone(demoAccount);
+        return account;
     }
 
     constructor(props: Props) {
         super(props);
-        this.state = {hasError: false, ...Login.getDefaultAccount()};
-    }
-
-    pickAccount() {
-        return pick(this.state, ['email', 'password']);
-    }
-
-    handleInputChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-        const target = event.target;
-        this.setState(() => ({
-            [target.name]: target.value
-        }));
+        this.state = { hasError: false, ...Login.getDefaultAccount() };
     }
 
     render() {
-        const {doLogin} = this.props;
+        const { doLogin } = this.props;
         return (
             <div className=" Login section">
                 <div className="modal is-active">
-                    <div className="modal-background"/>
+                    <div className="modal-background" />
                     <div className="modal-content">
                         <p className="title is-4">Please login ...</p>
                         <div className="columns is-mobile is-marginless is-multiline">
                             <div className="column is-full-mobile is-half-tablet">
-                                <InputField fieldClass={''} controlClass={'has-icons-left'}
-                                            name="email" type="email" placeholder="Email"
-                                            PreIcon={Icons.Mail}
-                                            value={this.state.email}
-                                            rules={[Rules.Required]}
-                                            handleChange={e => this.handleInputChange(e)}
-                                            handleKeyUp={this.handleKeyUp}
-                                            checkValidateStatus={this.checkValidateStatus}
+                                <InputField
+                                    fieldClass={''}
+                                    controlClass={'has-icons-left'}
+                                    name="email"
+                                    type="email"
+                                    placeholder="Email"
+                                    PreIcon={Icons.Mail}
+                                    value={this.state.email}
+                                    rules={[Rules.Required]}
+                                    handleChange={e =>
+                                        this.handleInputChange(e)
+                                    }
+                                    handleKeyUp={this.handleKeyUp}
+                                    setValidateStatus={this.checkValidateStatus}
                                 />
                             </div>
                             <div className="column is-full-mobile is-half-tablet">
-                                <InputField fieldClass={''} controlClass={'has-icons-left'}
-                                            name="password" type="password" placeholder="Password"
-                                            PreIcon={Icons.Lock}
-                                            value={this.state.password}
-                                            rules={[Rules.Required]}
-                                            handleChange={e => this.handleInputChange(e)}
-                                            handleKeyUp={this.handleKeyUp}
-                                            checkValidateStatus={this.checkValidateStatus}
+                                <InputField
+                                    fieldClass={''}
+                                    controlClass={'has-icons-left'}
+                                    name="password"
+                                    type="password"
+                                    placeholder="Password"
+                                    PreIcon={Icons.Lock}
+                                    value={this.state.password}
+                                    rules={[Rules.Required]}
+                                    handleChange={e =>
+                                        this.handleInputChange(e)
+                                    }
+                                    handleKeyUp={this.handleKeyUp}
+                                    setValidateStatus={this.checkValidateStatus}
                                 />
                             </div>
                             <div className="column">
                                 <div className="field is-grouped is-grouped-right">
                                     <div className="control">
-                                        <button className="login button is-success" onClick={this.beforeLogin}>
+                                        <button
+                                            className="login button is-success"
+                                            onClick={this.beforeLogin}
+                                        >
                                             Login
                                         </button>
                                     </div>
                                     <div className="control">
-                                        <button className="button is-center is-link" onClick={e => doLogin()}>
-                                            <span className="is-size-7">Anonymous</span>
+                                        <button
+                                            className="button is-center is-link"
+                                            onClick={e => doLogin()}
+                                        >
+                                            <span className="is-size-7">
+                                                Anonymous
+                                            </span>
                                         </button>
                                     </div>
                                 </div>
@@ -103,26 +110,33 @@ export default class Login extends React.Component<Props, State> {
         );
     }
 
-    handleKeyUp: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    handleInputChange: React.ChangeEventHandler<HTMLInputElement> = event => {
+        const target = event.target;
+        this.setState(() => ({
+            [target.name]: target.value
+        }));
+    };
+
+    handleKeyUp: React.KeyboardEventHandler<HTMLInputElement> = e => {
         if (e.key === 'Enter') {
             this.props.doLogin(this.state);
         }
-    }
+    };
 
     beforeLogin = (e?: React.MouseEvent<HTMLButtonElement>): void => {
-        if (!this.state.hasError) {
+        if (this.state.hasError) {
             Notification.error('Please check all error inputs!');
             return;
         }
-        const {email} = this.state;
-        const {doLogin} = this.props;
+        const { email } = this.state;
+        const { doLogin } = this.props;
         if (email) {
             localStorage.setItem('lastEmail', email);
         }
         doLogin(this.state);
-    }
+    };
 
-    checkValidateStatus = (isValid) => {
-        this.setState({hasError: isValid});
-    }
+    checkValidateStatus = hasError => {
+        this.setState({ hasError });
+    };
 }
