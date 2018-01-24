cb8f46155de1004a0323849eeb145cb9823dc706
lxzxl <lxz318@aliyun.com>
2018-01-24 11:01:35 +0530

    add signing loading bar.

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -6,64 +6,72 @@ import Notification from 'react-bulma-notification';
 import Main from './Main';
 import Login from './Login';
 
-import {app, AuthApi, CollectionsApi} from '../api';
+import { app, AuthApi, CollectionsApi } from '../api';
 
 interface State {
-    isLogin: boolean;
+  isLoading: boolean;
+  isLogin: boolean;
 }
 
 class App extends React.Component<{}, State> {
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            isLogin: !!app.auth().currentUser
-        };
-    }
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      isLogin: !!app.auth().currentUser
+    };
+  }
 
-    stopAuthStateListener?(): void;
+  stopAuthStateListener?(): void;
 
-    componentDidMount() {
-        this.stopAuthStateListener = app.auth().onAuthStateChanged((user: wilddog.User) => {
-            CollectionsApi.setRef();
-            this.setState({isLogin: !!user});
-        });
-        Notification.error('test', {duration: 0});
-    }
+  componentDidMount() {
+    this.stopAuthStateListener = app
+      .auth()
+      .onAuthStateChanged((user: wilddog.User) => {
+        CollectionsApi.setRef();
+        this.setState({ isLoading: false });
+        this.setState({ isLogin: !!user });
+      });
+  }
 
-    componentWillUnmount() {
-        if (this.stopAuthStateListener) {
-            this.stopAuthStateListener();
-        }
+  componentWillUnmount() {
+    if (this.stopAuthStateListener) {
+      this.stopAuthStateListener();
     }
+  }
 
-    render() {
-        const {isLogin} = this.state;
-        return isLogin ?
-            (
-                <div className="App">
-                    <Header doLogout={this.handleLogout}/>
-                    <Main/>
-                    <Footer/>
-                </div>
-            ) : <Login doLogin={this.handleLogin}/>;
-    }
+  render() {
+    const { isLogin, isLoading } = this.state;
+    return isLogin ? (
+      <div className="App">
+        <Header doLogout={this.handleLogout} />
+        <Main />
+        <Footer />
+      </div>
+    ) : (
+      <Login isLoading={isLoading} doLogin={this.handleLogin} />
+    );
+  }
 
-    handleLogin = async (data?: { email: string, password: string }) => {
-        try {
-            await (data ? AuthApi.signIn(data.email, data.password) : AuthApi.signInAnonymously());
-            Notification.success('Welcome, you are logged in!');
-        } catch (err) {
-            Notification.error(err.message);
-        }
+  handleLogin = async (data?: { email: string; password: string }) => {
+    try {
+      this.setState({ isLoading: true });
+      await (data
+        ? AuthApi.signIn(data.email, data.password)
+        : AuthApi.signInAnonymously());
+      Notification.success('Welcome, you are logged in!');
+    } catch (err) {
+      Notification.error(err.message);
     }
+  };
 
-    handleLogout = () => {
-        AuthApi.signOut().then(() => {
-            this.setState({
-                isLogin: false
-            });
-        });
-    }
+  handleLogout = () => {
+    AuthApi.signOut().then(() => {
+      this.setState({
+        isLogin: false
+      });
+    });
+  };
 }
 
 export default App;
--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -5,149 +5,147 @@ import Notification from 'react-bulma-notification';
 import { Rules, InputField } from '../components/InputField';
 
 interface Account {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 interface Props {
-    doLogin(data?: State): void;
+  isLoading: boolean;
+  doLogin(data?: State): void;
 }
 
 interface State extends Account {
-    hasError: boolean;
+  hasError: boolean;
 }
 
 const demoAccount = {
-    email: 'demo@demo.com',
-    password: 'demo123'
+  email: 'demo@demo.com',
+  password: 'demo123'
 };
 
 export default class Login extends React.Component<Props, State> {
-    private inputRefs: Array<InputField> = [];
-    static getDefaultAccount(): Account {
-        const lastEmail = localStorage.getItem('lastEmail');
-        const account = clone(demoAccount);
-        if (lastEmail && lastEmail !== account.email) {
-            account.email = lastEmail;
-            account.password = '';
-        }
-        return account;
+  private inputRefs: Array<InputField> = [];
+  static getDefaultAccount(): Account {
+    const lastEmail = localStorage.getItem('lastEmail');
+    const account = clone(demoAccount);
+    if (lastEmail && lastEmail !== account.email) {
+      account.email = lastEmail;
+      account.password = '';
     }
+    return account;
+  }
 
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            hasError: false,
-            ...Login.getDefaultAccount()
-        };
-    }
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      ...Login.getDefaultAccount()
+    };
+  }
 
-    render() {
-        const { doLogin } = this.props;
-        return (
-            <div className=" Login section">
-                <div className="modal is-active">
-                    <div className="modal-background" />
-                    <div className="modal-content">
-                        <p className="title is-4">Please login ...</p>
-                        <div className="columns is-mobile is-marginless is-multiline">
-                            <div className="column is-full-mobile is-half-tablet">
-                                <InputField
-                                    ref={this.collectInputs}
-                                    fieldClass={''}
-                                    controlClass={'has-icons-left'}
-                                    name="email"
-                                    type="email"
-                                    placeholder="Email"
-                                    PreIcon={Icons.Mail}
-                                    value={this.state.email}
-                                    rules={[Rules.Required]}
-                                    handleChange={this.handleInputChange}
-                                    handleKeyUp={this.handleKeyUp}
-                                    setValidateStatus={this.setValidateStatus}
-                                />
-                            </div>
-                            <div className="column is-full-mobile is-half-tablet">
-                                <InputField
-                                    ref={this.collectInputs}
-                                    fieldClass={''}
-                                    controlClass={'has-icons-left'}
-                                    name="password"
-                                    type="password"
-                                    placeholder="Password"
-                                    PreIcon={Icons.Lock}
-                                    value={this.state.password}
-                                    rules={[Rules.Required]}
-                                    handleChange={this.handleInputChange}
-                                    handleKeyUp={this.handleKeyUp}
-                                    setValidateStatus={this.setValidateStatus}
-                                />
-                            </div>
-                            <div className="column">
-                                <div className="field is-grouped is-grouped-right">
-                                    <div className="control">
-                                        <button
-                                            className="login button is-success"
-                                            onClick={this.beforeLogin}
-                                        >
-                                            Login
-                                        </button>
-                                    </div>
-                                    <div className="control">
-                                        <button
-                                            className="button is-center is-link"
-                                            onClick={e => doLogin()}
-                                        >
-                                            <span className="is-size-7">
-                                                Anonymous
-                                            </span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  render() {
+    const { isLoading, doLogin } = this.props;
+    return (
+      <div className="Login section">
+        <div className="modal is-active">
+          <div className="modal-background" />
+          <div className="modal-content loading">
+            <p className="title is-4">Please login ...</p>
+            <div className="columns is-mobile is-marginless is-multiline">
+              <div className="column is-full-mobile is-half-tablet">
+                <InputField
+                  ref={this.collectInputs}
+                  fieldClass={''}
+                  controlClass={'has-icons-left'}
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  PreIcon={Icons.Mail}
+                  value={this.state.email}
+                  rules={[Rules.Required]}
+                  handleChange={this.handleInputChange}
+                  handleKeyUp={this.handleKeyUp}
+                  setValidateStatus={this.setValidateStatus}
+                />
+              </div>
+              <div className="column is-full-mobile is-half-tablet">
+                <InputField
+                  ref={this.collectInputs}
+                  fieldClass={''}
+                  controlClass={'has-icons-left'}
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  PreIcon={Icons.Lock}
+                  value={this.state.password}
+                  rules={[Rules.Required]}
+                  handleChange={this.handleInputChange}
+                  handleKeyUp={this.handleKeyUp}
+                  setValidateStatus={this.setValidateStatus}
+                />
+              </div>
+              <div className="column">
+                <div className="field is-grouped is-grouped-right">
+                  <div className="control">
+                    <button className="login button is-success" onClick={this.beforeLogin}>
+                      Login
+                    </button>
+                  </div>
+                  <div className="control">
+                    <button className="button is-center is-link" onClick={e => doLogin()}>
+                      <span className="is-size-7">Anonymous</span>
+                    </button>
+                  </div>
                 </div>
+              </div>
             </div>
-        );
+            {isLoading && (
+              <div className="loading">
+                <div className="loading_bar progress" />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  collectInputs = (input: InputField) => {
+    this.inputRefs = [...this.inputRefs, input];
+  };
+
+  handleInputChange: React.ChangeEventHandler<HTMLInputElement> = event => {
+    const target = event.target;
+    this.setState(() => ({
+      [target.name]: target.value
+    }));
+  };
+
+  handleKeyUp: React.KeyboardEventHandler<HTMLInputElement> = e => {
+    if (e.key === 'Enter') {
+      this.beforeLogin();
     }
+  };
 
-    collectInputs = (input: InputField) => {
-        this.inputRefs = [...this.inputRefs, input];
-    };
+  beforeLogin = () => {
+    // do validating
+    const isValid = this.inputRefs.every(input => {
+      return input.checkIsValid();
+    });
+    this.setState({ hasError: !isValid });
+    if (!isValid) {
+      Notification.error('Please check all error inputs!');
+      return;
+    }
+    const { email } = this.state;
+    const { doLogin } = this.props;
+    if (email) {
+      localStorage.setItem('lastEmail', email);
+    }
+    doLogin(this.state);
+  };
 
-    handleInputChange: React.ChangeEventHandler<HTMLInputElement> = event => {
-        const target = event.target;
-        this.setState(() => ({
-            [target.name]: target.value
-        }));
-    };
-
-    handleKeyUp: React.KeyboardEventHandler<HTMLInputElement> = e => {
-        if (e.key === 'Enter') {
-            this.beforeLogin();
-        }
-    };
-
-    beforeLogin = () => {
-        // do validating
-        const isValid = this.inputRefs.every(input => {
-            return input.checkIsValid();
-        });
-        this.setState({ hasError: !isValid });
-        if (!isValid) {
-            Notification.error('Please check all error inputs!');
-            return;
-        }
-        const { email } = this.state;
-        const { doLogin } = this.props;
-        if (email) {
-            localStorage.setItem('lastEmail', email);
-        }
-        doLogin(this.state);
-    };
-
-    setValidateStatus = isValid => {
-        this.setState({ hasError: !isValid });
-    };
+  setValidateStatus = isValid => {
+    this.setState({ hasError: !isValid });
+  };
 }
